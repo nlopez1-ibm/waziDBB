8f72b00423ecb04cbc65fa1baa286f144a181b51
nlopez <nelson.lopez1@ibm.com>
2024-04-12 21:20:28 -0400

    Mod UCD_Pub to support arty and codestation dual mode see AzDBB pipe

--- dbb-v2/dbb-zappbuild/scripts/CD/UCD_Pub.sh
+++ dbb-v2/dbb-zappbuild/scripts/CD/UCD_Pub.sh
@@ -1,22 +1,31 @@
 #!/bin/sh
+# mod njl 4/21/24 - dual mode Artifactory or CodeStation for Demos 
 . ~/.profile
 
 ucd_version=$1
 ucd_Component_Name=$2
 MyWorkDir=$3
-artifacts=buildReport.json
+ArtifactoryMode=$4
 
 buzTool=~/ibm-ucd/agent/bin/buztool.sh
 pub=$DBB_HOME/dbb-zappbuild/scripts/UCD/dbb-ucd-packaging.groovy  
 
+artProp=""
+if [ -z "$ArtifactoryMode" ]; then
+    artStore="UCD CodeStation"     
+else
+    artStore="jFroj" 
+    artProp=" -prop $MyWorkDir/artifactoryProps"
+fi
 
 echo "**************************************************************"
 echo "**  Started:  UCD_Pub.sh (V2) Pack&Pub on HOST/USER: $(uname -Ia) $USER"
 echo "**                           Version/Build_ID:" $ucd_version
 echo "**                                 Component:" $ucd_Component_Name 
 echo "**                                   workDir:" $MyWorkDir   
-echo "**                         DBB Artifact Path:" $artifacts
 echo "**                              BuzTool Path:" $buzTool 
 echo "**                          Packaging Script:" $pub 
+echo "**                            Artifact Store:" $artStore                   
 
-groovyz $pub  --buztool $buzTool --workDir $MyWorkDir  --component $ucd_Component_Name --versionName $ucd_version
+groovyz $pub  --buztool $buzTool --workDir $MyWorkDir  --component $ucd_Component_Name --versionName $ucd_version $artProp
+exit $?
